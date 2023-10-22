ed4590ef03d089cdea9f4dbc965fb4affad3adc0
miguelvalden <miguelvalden77@gmail.com>
2023-10-22 06:05:01 +0200

    update populate author

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,86 +4,92 @@ const Comment = require("../models/Post.model")
 const User = require("../models/User.model")
 const isAuth = require("../middlewares/isAuth")
 
-router.post("/:id/create", isAuth, async (req, res, next)=>{
-    
-    const {title, picture, description, owner} = req.body
-    const {id} = req.params
+router.post("/:id/create", isAuth, async (req, res, next) => {
+
+    const { title, picture, description, owner } = req.body
+    const { id } = req.params
 
     console.log(picture)
 
-    try{
-        const post = await Post.create({title, picture, description, owner})
-        await User.findByIdAndUpdate(id, {$addToSet:{posts: post._id}})
+    try {
+        const post = await Post.create({ title, picture, description, owner })
+        await User.findByIdAndUpdate(id, { $addToSet: { posts: post._id } })
         res.json(post)
     }
-    catch(error){
+    catch (error) {
         next(error)
     }
 
 })
 
-router.get("/all", isAuth, async (req, res, next)=>{
+router.get("/all", isAuth, async (req, res, next) => {
 
-    try{
-        const posts = await Post.find().populate("comments")
+    try {
+        const posts = await Post.find().populate({
+            path: 'comments',
+            populate: {
+                path: 'owner',
+                model: 'User' // El modelo de autor
+            }
+        })
         res.json(posts)
     }
-    catch(error){
+    catch (error) {
         next(error)
     }
 
 })
 
-router.patch("/:id/edit", isAuth, async (req, res, next)=>{
-    const {id} = req.params
-    const {title, picture, description} = req.body
+router.patch("/:id/edit", isAuth, async (req, res, next) => {
+    const { id } = req.params
+    const { title, picture, description } = req.body
 
-    try{
-        await Post.findByIdAndUpdate(id, {title, picture, description})
+    try {
+        await Post.findByIdAndUpdate(id, { title, picture, description })
         res.json("Post modificado")
     }
-    catch(error){
+    catch (error) {
         next(error)
-        res.json({errorMessage: "Error al obtener los post"})
+        res.json({ errorMessage: "Error al obtener los post" })
     }
 
 })
 
-router.delete("/:id/delete", isAuth, async (req, res, next)=>{
+router.delete("/:id/delete", isAuth, async (req, res, next) => {
 
-    const {id} = req.params
+    const { id } = req.params
 
-    try{
+    try {
 
         const post = await Post.findById(id).populate("comments")
         //const post2 = await Post.findById(id).populate("owner")
-        await User.findByIdAndUpdate(post.owner, {$pull:{posts: id}})
+        await User.findByIdAndUpdate(post.owner, { $pull: { posts: id } })
         await Post.findByIdAndDelete(id)
         // Pasar id del user y borrar el post también de su array de posts
         console.log(post.comments)
 
-        if(post.comments.length > 0){
+        if (post.comments.length > 0) {
             await Comment.deleteMany(post.comments)
             return
         }
 
-        
+
         res.json(post)
 
     }
-    catch(error){
-        res.json({errorMessage: "Error al borrar el post"})
+    catch (error) {
+        res.json({ errorMessage: "Error al borrar el post" })
     }
 })
 
-router.get("/:id", async (req, res, next)=>{
-    const {id} = req.params
+router.get("/:id", async (req, res, next) => {
+    const { id } = req.params
 
-    try{
+    try {
         const onlyPost = await Post.findById(id).populate("owner")
         res.json(onlyPost)
     }
-    catch(error){
+    catch (error) {
         next(error)
     }
 
