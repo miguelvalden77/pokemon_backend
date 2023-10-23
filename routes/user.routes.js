03e498713648246ad8ce59a4bf8fb959df208475
miguelvalden <miguelvalden77@gmail.com>
2023-10-23 07:28:42 +0200

    allPokemons route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,25 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/User.model")
 
-router.post("/add/pokemon", async (req, res, next)=>{
+router.post("/add/pokemon", async (req, res, next) => {
 
-    const {userId, name} = req.body
+    const { userId, name } = req.body
 
-    try{
-        await User.findByIdAndUpdate(userId, {$addToSet: {pokemons: name}})
+    try {
+        await User.findByIdAndUpdate(userId, { $addToSet: { pokemons: name } })
+        console.log(name)
         res.json("Pokemon añadido")
     }
-    catch(error){
+    catch (error) {
         next(error)
     }
 
 })
 
-router.get("/:id", async (req, res, next)=>{
-    
-    const {id} = req.params
+router.get("/:id", async (req, res, next) => {
 
-    try{
+    const { id } = req.params
+
+    try {
         const user = User.findById(id).populate("posts")
         // Obtener posts haciendo peticion a los posts por su id (están en el payload)
 
@@ -32,29 +33,42 @@ router.get("/:id", async (req, res, next)=>{
             posts: user.posts
         }
 
-        const authToken = jwt.sign(payload, process.env.SECRET_KEY, {algorithm: "HS256", expiresIn: "4h"})
+        const authToken = jwt.sign(payload, process.env.SECRET_KEY, { algorithm: "HS256", expiresIn: "4h" })
 
-        res.json({authToken: authToken})
+        res.json({ authToken: authToken })
         //res.json(user)
     }
-    catch(error){
+    catch (error) {
         next(error)
     }
 
 })
 
-router.post("/:name/pokemon", async (req, res, next)=>{
-    const {name} = req.params
-    const {id} = req.body
+router.post("/:name/pokemon", async (req, res, next) => {
+    const { name } = req.params
+    const { id } = req.body
 
-    try{
-        console.log(id)
-        const usuario = await User.findByIdAndUpdate(id, {$pull:{pokemons: name}})
+    try {
+        console.log({ name })
+        const usuario = await User.findByIdAndUpdate(id, { $pull: { pokemons: name } })
         console.log(usuario, "EJEMPLO")
         res.json(usuario)
     }
-    catch(error){
+    catch (error) {
         next(error)
+    }
+})
+
+router.get("/getAllPokemon/:userId", async (req, res) => {
+
+    const { userId } = req.params
+
+    try {
+        const pokemons = await User.findById(userId).select("pokemons")
+        res.json(pokemons)
+    }
+    catch (err) {
+        console.log(err)
     }
 })
 
